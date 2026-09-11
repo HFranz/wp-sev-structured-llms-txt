e221b8987059d3600d46df70f50b2d1abdd4124f
Heinrich Franz <heinrich.franz@sevmatic.com>
2026-09-11 10:28:11 +0000

    Remove unnecessary Domain Path header
    The plugin is listed on the official WordPress Plugin Directory, where the Domain Path header can be omitted per the WordPress i18n documentation.

--- sev-structured-llms-txt.php
+++ sev-structured-llms-txt.php
@@ -11,7 +11,6 @@
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: sev-structured-llms-txt
- * Domain Path: /languages
  *
  * php version 8.0
  *
